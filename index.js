869ce6c009185020d63798cde0618f19bdf36647
Jim <jadavey@hotmail.com>
2021-05-18 21:41:49 +0100

    Weather API Post Working

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 const path = require("path");
+const bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/indexRouter');
-const weatherRouter = require('./routes/weatherRouter');
-const errRouter = require('./routes/errRouter');
+const indexRouter = require("./routes/indexRouter");
+const weatherRouter = require("./routes/weatherRouter");
+const errRouter = require("./routes/errRouter");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -22,17 +26,14 @@ app.engine(
 );
 
 app.set("view engine", ".hbs");
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/weather', weatherRouter);
-app.use('*', errRouter);
+app.use("/", indexRouter);
+app.use("/weather", weatherRouter);
+app.use("*", errRouter);
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
-
-
 
 app.listen(3000, () => {
   console.log("Listening to port 3000");
--- routes/weatherRouter.js
+++ routes/weatherRouter.js
@@ -1,4 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const getWeatherInfo = require("../lib/weatherinfo");
+
+router.get("/", (req, res) => {
+  res.render("weather");
+});
+
+router.post("/", async (req, res) => {
+  let location = req.body.city;
+  let countryCode = req.body.countryCode;
+  let data = await getWeatherInfo(location, countryCode);
+  if (data.cod == "404") {
+    res.render("weather", {
+      err: `The provided location doesn't exist ${location}, ${countryCode}`,
+    });
+    return;
+  }
+  let name = data.name;
+  let description = data.weather[0].description;
+  let temp = data.main.temp;
+  let feels_like = data.main.feels_like;
+  res.render("weather", {
+    name,
+    data: { description, temp, feels_like },
+    listExists: true,
+  });
+});
 
 module.exports = router;
